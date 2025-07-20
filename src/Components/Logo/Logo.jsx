f725bf02acc6cc48b6523d5f1296e2f9753e5e70
712 <khamidullayew@gmail.com>
2025-07-21 00:49:10 +0500

    "logo fixed"

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div>
-      <h1 className="text font-bold text-[32px] max-[650px]:text-[25px] hover:cursor-pointer">
+      <h1 className="title font-bold text-[32px] max-[650px]:text-[25px] hover:cursor-pointer">
         SHOP.CO
       </h1>
     </div>
